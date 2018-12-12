f7d80aa3d6ee1e9d4a760ab7319c21ba1e4a88e6
Flo <Flo@moringa.com>
2018-12-12 20:14:27 +0300

    Add initial files and file structures

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+Game rules:
+The goal of the game is to be the first player to get 100 points or more.
+The game has 2 players; players take turns rolling a six-sided die.
+If a 1 is rolled, then the player gets no points and it becomes the other player's turn.
+If the player rolls any other number, i.e. 2 through 6, it is added to their turn total and the player's turn continues
+If a player chooses to "hold", their turn total is added to their score, and it becomes the next player's turn
+The first player to score 100 or more points wins
+The purpose of this code is to make a program that gives the players options to roll again or hold, generate random numbers, and also be able to add and display each player's scores
+*/
